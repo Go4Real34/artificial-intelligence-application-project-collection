a96fa21f0159f3789e08deb7fed1e222068ed589
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-02-27 19:43:05 +0300

    add DFS class __init__ function implementation similar to BFS

--- Artificial Intelligence Applications Project Collection/src/bfs_dfs_astar/DFS.py
+++ Artificial Intelligence Applications Project Collection/src/bfs_dfs_astar/DFS.py
@@ -1,4 +1,14 @@
-class DFS:
-    def __init__(self):
-        pass
+from ..structures import Node, Stack
+
+class DFS():
+    def __init__(self, maze):
+        self.maze = maze
+        self.number_of_explored_nodes = 0
+        self.explored_nodes = set()
+        
+        self.stack = Stack()
+        start = Node(self.maze.start, None, None)
+        self.stack.push(start)
+        
+        return
     
