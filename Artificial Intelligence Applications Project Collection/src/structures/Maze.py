845f35a705282b441d0b07cfeeeb2386a552208b
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-02-27 18:01:34 +0300

    add Maze class get_maze_properties function implementation

--- Artificial Intelligence Applications Project Collection/src/structures/Maze.py
+++ Artificial Intelligence Applications Project Collection/src/structures/Maze.py
@@ -3,6 +3,7 @@ class Maze:
         file_content = self.read_and_save_file_content(file_name)
         file_content_lines = self.split_file_content_by_lines(file_content)
         self.width, self.height = self.get_maze_width_and_height(file_content_lines)
+        self.start, self.goal, self.walls = self.get_maze_properties(file_content_lines)
     
     def read_and_save_file(self, file_name):
         with open(file_name) as file:
@@ -24,4 +25,29 @@ class Maze:
         height = len(file_content_lines)
         
         return width, height
+    
+    def get_maze_properties(self, file_content_lines):
+        walls = []
+        for row in range(self.height):
+            row = []
+            for column in range(self.width):
+                try:
+                    if file_content_lines[row][column] == "A":
+                        start = (row, column)
+                        
+                    elif file_content_lines[row][column] == "B":
+                        goal = (row, column)
+                        
+                    elif file_content_lines[row][column] == " ":
+                        row.append(False)
+                        
+                    else:
+                        row.append(True)
+                        
+                except IndexError:
+                    row.append(False)
+                    
+            walls.append(row)
+            
+        return start, goal, walls
     
