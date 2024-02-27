c8547cee7399551c2cc287ff134656a153d2d758
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-02-27 18:29:59 +0300

    add BFS class __init__ function implementation

--- Artificial Intelligence Applications Project Collection/src/bfs_dfs_astar/BFS.py
+++ Artificial Intelligence Applications Project Collection/src/bfs_dfs_astar/BFS.py
@@ -1,4 +1,14 @@
+from ..structures import Node, Queue
+
 class BFS:
-    def __init__(self):
-        pass
+    def __init__(self, maze):
+        self.maze = maze
+        self.number_of_explored_nodes = 0
+        self.explored_nodes = set()
+        
+        self.frontier = Queue()
+        start = Node(self.maze.start, None, None)
+        self.frontier.push(start)
+        
+        return
     
