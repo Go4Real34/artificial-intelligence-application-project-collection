6f8232948c9e1344135eedd0ec3d82afa291f26a
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-02-27 19:42:05 +0300

    add overwritten pop function implementation of Queue to Stack class

--- Artificial Intelligence Applications Project Collection/src/structures/Stack.py
+++ Artificial Intelligence Applications Project Collection/src/structures/Stack.py
@@ -1,4 +1,12 @@
-class Stack:
-    def __init__(self):
-        pass
+from .Queue import Queue
+
+class Stack(Queue):
+    def pop(self):
+        if self.is_empty():
+            raise Exception("Cannot pop Node object: Stack frontier is empty!")
+        
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
     
