553a46ebd54a878e37dbd696976e4d4a015e42bc
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-02-27 17:55:35 +0300

    add Maze class read_and_save_file function implementation

--- Artificial Intelligence Applications Project Collection/src/structures/Maze.py
+++ Artificial Intelligence Applications Project Collection/src/structures/Maze.py
@@ -1,4 +1,16 @@
 class Maze:
-    def __init__(self):
-        pass
+    def __init__(self, file_name):
+        file_content = self.read_and_save_file_content(file_name)
+    
+    def read_and_save_file(self, file_name):
+        with open(file_name) as file:
+            file_content = file.read().upper()
+            
+        if file_content.count("A") != 1:
+            raise Exception("Maze is invalid: There must be exactly one starting point!")
+        
+        if file_content.count("B") != 1:
+            raise Exception("Maze is invalid: There must be exactly one goal!")
+        
+        return file_content
     
