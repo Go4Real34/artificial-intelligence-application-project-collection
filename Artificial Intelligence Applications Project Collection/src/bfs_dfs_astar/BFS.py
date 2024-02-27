9e39b8468ab7498f1acd955db0fba359a1a7e2ed
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-02-27 19:20:30 +0300

    add BFS solve function implementation

--- Artificial Intelligence Applications Project Collection/src/bfs_dfs_astar/BFS.py
+++ Artificial Intelligence Applications Project Collection/src/bfs_dfs_astar/BFS.py
@@ -12,3 +12,32 @@ class BFS:
         
         return
     
+    def solve(self):
+        while True:
+            if self.frontier.is_empty():
+                raise Exception("Maze is invalid. No solution exists.")
+            
+            node = self.frontier.pop()
+            self.number_of_explored_nodes += 1
+            
+            if node.state == self.maze.goal:
+                actions = []
+                cells = []
+                while node.parent is not None:
+                    actions.append(node.action)
+                    cells.append(node.state)
+                    node = node.parent
+                
+                solution = (actions[::-1], cells[::-1])
+                self.maze.set_solution(solution)
+                return
+                
+            self.explored_nodes.add(node.state)
+            
+            for action, state in self.maze.get_available_actions(node.state):
+                if (not self.frontier.does_contain_state(state)) and (state not in self.explored_nodes):
+                    child = Node(state, node, action)
+                    self.frontier.push(child)
+                    
+        return
+    
