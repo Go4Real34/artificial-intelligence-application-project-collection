61efe9ae49658ff3f80dc35c394d4ffefe36e577
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-02-27 17:32:10 +0300

    add Queue class pop function implementation

--- Artificial Intelligence Applications Project Collection/src/structures/Queue.py
+++ Artificial Intelligence Applications Project Collection/src/structures/Queue.py
@@ -12,3 +12,12 @@ class Queue:
     def is_empty(self):
         return len(self.frontier) == 0
     
+    def pop(self):
+        if self.is_empty():
+            raise Exception("Cannot pop Node object: Queue frontier is empty!")
+        
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
+    
