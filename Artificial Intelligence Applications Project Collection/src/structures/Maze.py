877ae5f22f03e7e23e196c76bffebc56f3ea9a5d
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-02-27 17:59:55 +0300

    add Maze class split_file_content_by_lines function implementation

--- Artificial Intelligence Applications Project Collection/src/structures/Maze.py
+++ Artificial Intelligence Applications Project Collection/src/structures/Maze.py
@@ -1,6 +1,7 @@
 class Maze:
     def __init__(self, file_name):
         file_content = self.read_and_save_file_content(file_name)
+        file_content_lines = self.split_file_content_by_lines(file_content)
     
     def read_and_save_file(self, file_name):
         with open(file_name) as file:
@@ -13,4 +14,7 @@ class Maze:
             raise Exception("Maze is invalid: There must be exactly one goal!")
         
         return file_content
+    
+    def split_file_content_by_lines(self, file_content):
+        return file_content.splitlines()
     
