6bd0f0f03c2318e68edfe4df70e6ffe64e279352
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-02-27 18:00:32 +0300

    add Maze class get_maze_width_and_height function implementation

--- Artificial Intelligence Applications Project Collection/src/structures/Maze.py
+++ Artificial Intelligence Applications Project Collection/src/structures/Maze.py
@@ -2,6 +2,7 @@ class Maze:
     def __init__(self, file_name):
         file_content = self.read_and_save_file_content(file_name)
         file_content_lines = self.split_file_content_by_lines(file_content)
+        self.width, self.height = self.get_maze_width_and_height(file_content_lines)
     
     def read_and_save_file(self, file_name):
         with open(file_name) as file:
@@ -17,4 +18,10 @@ class Maze:
     
     def split_file_content_by_lines(self, file_content):
         return file_content.splitlines()
+    
+    def get_maze_width_and_height(self, file_content_lines):
+        width = max(len(line) for line in file_content_lines)
+        height = len(file_content_lines)
+        
+        return width, height
     
