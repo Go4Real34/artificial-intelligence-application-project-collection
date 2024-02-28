24ad2803e25abb0a8fcf0a50a2b5098046df16af
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-02-28 18:12:24 +0300

    add TicTacToe class print_board function implementation

--- Artificial Intelligence Applications Project Collection/src/structures/TicTacToe.py
+++ Artificial Intelligence Applications Project Collection/src/structures/TicTacToe.py
@@ -1,4 +1,18 @@
 class TicTacToe:
     def __init__(self):
-        pass
+        self.board = [
+            [' ', ' ', ' '],
+            [' ', ' ', ' '],
+            [' ', ' ', ' ']
+        ]
+        
+    def print_board(self):
+        for row_index, row in enumerate(self.board):
+            print('|'.join(row))
+            if row_index != 2:
+                print('-----')
+                
+        print()
+        
+        return
     
