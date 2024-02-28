e2797d04f1baebc7d5256b250da3f123cf52cb6b
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-02-28 17:38:55 +0300

    add ASTAR class __init__ function implementation

--- Artificial Intelligence Applications Project Collection/src/bfs_dfs_astar/ASTAR.py
+++ Artificial Intelligence Applications Project Collection/src/bfs_dfs_astar/ASTAR.py
@@ -1,4 +1,14 @@
+from ..structures import Node, Queue
+
 class ASTAR:
-    def __init__(self):
-        pass
+    def __init__(self, maze):
+        self.maze = maze
+        self.number_of_explored_nodes = 0
+        self.explored_nodes = set()
+        
+        self.queue = Queue()
+        start = (Node(self.maze.start, None, None), 0)
+        self.queue.push(start)
+        
+        return
     
