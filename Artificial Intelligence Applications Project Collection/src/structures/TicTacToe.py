fee8a0557e56a646a16f731a7d599918d6dfb0ba
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-02-29 00:23:36 +0300

    fix turn switch and winner check logic bugs in TicTacToe class

--- Artificial Intelligence Applications Project Collection/src/structures/TicTacToe.py
+++ Artificial Intelligence Applications Project Collection/src/structures/TicTacToe.py
@@ -33,29 +33,38 @@ class TicTacToe:
         
         return
     
-    def is_winner_exists(self):
+    def get_winner(self):
+        previous_player = self.players[(self.current_player_index - 1) % len(self.players)]
         for row in self.board:
             if row[0] == row[1] == row[2] != ' ':
-                return True
+                self.winner = previous_player
             
         for c in range(3):
             if self.board[0][c] == self.board[1][c] == self.board[2][c] != ' ':
-                return True
+                self.winner = previous_player
             
         if self.board[0][0] == self.board[1][1] == self.board[2][2] != ' ':
-            return True
+            self.winner = previous_player
         
         if self.board[0][2] == self.board[1][1] == self.board[2][0] != ' ':
-            return True
+            self.winner = previous_player
         
-        return False
+        if self.winner is None and len(self.available_moves) == 0:
+            self.winner = "tie"
         
-    def next_turn(self):
-        random_move = random_choice(self.available_moves)
-        self.available_moves.remove(random_move)
-        r, c = random_move
-        self.board[r][c] = self.current_player
-        self.current_player = self.players[(self.current_player_index + 1) % len(self.players)]
+        return self.winner
         
+    def next_turn(self, move):
+        if move in self.available_moves:
+            self.available_moves.remove(move)
+            r, c = move
+            self.board[r][c] = self.current_player
+            self.current_player_index += 1
+            self.current_player = self.players[self.current_player_index % len(self.players)]
+        
+        else:
+            print('Invalid move', end='')
+            print("\n" * 3, end='')
+
         return
     
