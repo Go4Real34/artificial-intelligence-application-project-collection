f6aaa9bd029fb28f5edc5750e5d5666504f7962b
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-02-29 03:25:30 +0300

    add Maze class save_maze_solution_as_file functionality with solutions

--- Artificial Intelligence Applications Project Collection/src/structures/Maze.py
+++ Artificial Intelligence Applications Project Collection/src/structures/Maze.py
@@ -1,10 +1,19 @@
+from shlex import join
+from PIL import Image, ImageDraw
+from os import makedirs as make_directories, remove as remove_file
+from os.path import exists as does_path_exist, join as join_paths
+
 class Maze:
     def __init__(self, file_name):
-        file_content = self.read_and_save_file(file_name)
+        self.file_name = file_name
+        file_content = self.read_and_save_file(self.file_name)
         file_content_lines = self.split_file_content_by_lines(file_content)
         self.width, self.height = self.get_maze_width_and_height(file_content_lines)
         self.start, self.goal, self.walls = self.get_maze_properties(file_content_lines)
         self.solution = None
+        
+        self.explored_nodes = None
+        self.file_save_location = None
         
         return
     
@@ -56,6 +65,68 @@ class Maze:
         
         return
     
+    def set_explored_nodes(self, explored_nodes):
+        self.explored_nodes = explored_nodes
+        return
+    
+    def set_file_save_location(self, file_save_location):
+        self.file_save_location = file_save_location
+        return
+    
+    def save_maze_solution_as_file(self, used_algorithm_name):
+        cell_size = 50
+        cell_border = 2
+
+        image = Image.new(
+            "RGBA", 
+            (self.width * cell_size, self.height * cell_size), 
+            "black"
+        )
+        draw = ImageDraw.Draw(image)
+        
+        solution = self.solution[1] if self.solution is not None else None
+        for r, row in enumerate(self.walls):
+            for c, column in enumerate(row):
+                if column:
+                    fill = (40, 40, 40)
+                    
+                elif (r, c) == self.start:
+                    fill = (255, 0, 0)
+                    
+                elif (r, c) == self.goal:
+                    fill = (0, 171, 28)
+                    
+                elif solution is not None and (r, c) in solution:
+                    fill = (220, 235, 113)
+                    
+                elif solution is not None and (r, c) in self.explored_nodes:
+                    fill = (212, 97, 85)
+                    
+                else:
+                    fill = (237, 240, 252)
+                    
+                draw.rectangle(
+                    ([(c * cell_size + cell_border, r * cell_size + cell_border), 
+                      ((c + 1) * cell_size - cell_border, (r + 1) * cell_size - cell_border)]), 
+                      fill = fill
+                )
+                
+        if (self.file_save_location is not None) or (self.explored_nodes is not None):
+            file_full_save_location = join_paths(
+                self.file_save_location, 
+                self.file_name.split("\\")[-1].rstrip(".txt") + "_txt_with_" + used_algorithm_name + ".png"
+            )
+            if does_path_exist(file_full_save_location):
+                remove_file(file_full_save_location)
+                
+            else:
+                if not does_path_exist(self.file_save_location):
+                    make_directories(self.file_save_location)
+                
+            image.save(file_full_save_location)
+            print("Solution saved at:", file_full_save_location)
+        return
+
     def read_and_save_file(self, file_name):
         with open(file_name) as file:
             file_content = file.read().upper()
--- Artificial Intelligence Applications Project Collection/src/tests/bfs_dfs_astar_test_main.py
+++ Artificial Intelligence Applications Project Collection/src/tests/bfs_dfs_astar_test_main.py
@@ -6,11 +6,11 @@ from ..bfs_dfs_astar import BFS, DFS, ASTAR
 
 def bfs_dfs_astar_test():
     cwd = get_current_working_directory()
-    maze_folder = join_paths(cwd, 'src', 'tests', 'mazes')
-    current_mazes = list_directory(maze_folder)
+    mazes_folder = join_paths(cwd, 'src', 'tests', 'mazes')
+    current_mazes = list_directory(mazes_folder)
     
     print("Current Mazes:\n")
-    for i, maze in enumerate(current_mazes):
+    for i, maze in enumerate([maze for maze in current_mazes if maze.endswith(".txt")]):
         print(f"[{i + 1}] - {maze}")
     
     print()
@@ -18,7 +18,8 @@ def bfs_dfs_astar_test():
     print()
     try:
         if 0 < int(maze_index) <= len(current_mazes) + 1:
-            maze = Maze(join_paths(maze_folder, current_mazes[int(maze_index) - 1]))
+            maze_file_name = current_mazes[int(maze_index) - 1]
+            maze = Maze(join_paths(mazes_folder, maze_file_name))
             print("[1] - BFS (Breadth First Search)")
             print("[2] - DFS (Depth First Search)")
             print("[3] - A* (A Star)")
@@ -26,13 +27,17 @@ def bfs_dfs_astar_test():
             algorithm_index = input("\nWhich algorithm do you want to use?: ")
             try:
                 algorithm = None
+                algorithm_name = None
                 if 0 < int(algorithm_index) <= 3:
                     if int(algorithm_index) == 1:
                         algorithm = BFS(maze)
+                        algorithm_name = "bfs"
                     elif int(algorithm_index) == 2:
                         algorithm = DFS(maze)
+                        algorithm_name = "dfs"
                     elif int(algorithm_index) == 3:
                         algorithm = ASTAR(maze)
+                        algorithm_name = "astar"
                         
                     print()
                     
@@ -41,11 +46,15 @@ def bfs_dfs_astar_test():
                     
                     print("Solving started.")
                     algorithm.solve()
+                    maze.set_explored_nodes(algorithm.explored_nodes)
                     
                     print("\nNumber of nodes explored:", algorithm.number_of_explored_nodes)
 
                     print("\nSolution:")
                     maze.print_maze()
+                    
+                    maze.set_file_save_location(join_paths(mazes_folder, "solutions", maze_file_name.rstrip(".txt")))
+                    maze.save_maze_solution_as_file(algorithm_name)
                         
                 else:
                     print("Invalid index on algorithm selection.")
