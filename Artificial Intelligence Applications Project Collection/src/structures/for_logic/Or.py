8615360c5b7866619a0e227b9a1b9905697f6a78
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-03-09 18:40:42 +0300

    add Or class helper function implementations

--- Artificial Intelligence Applications Project Collection/src/structures/for_logic/Or.py
+++ Artificial Intelligence Applications Project Collection/src/structures/for_logic/Or.py
@@ -8,3 +8,13 @@ class Or(Sentence):
         self.disjuncts = list(disjuncts)
         return
     
+    def __eq__(self, other):
+        return (isinstance(other, Or)) and self.disjuncts == other.disjuncts
+    
+    def __hash__(self):
+        return hash(("or", tuple(hash(disjunct) for disjunct in self.disjuncts)))
+    
+    def __repr__(self):
+        disjunts = ", ".join([str(disjunct) for disjunct in self.disjuncts])
+        return f"Or({disjunts})"
+    
