77decd353f7d580a91fc382f278954d294426836
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-03-09 19:46:33 +0300

    add Implication helper function implementations

--- Artificial Intelligence Applications Project Collection/src/structures/for_logic/Implication.py
+++ Artificial Intelligence Applications Project Collection/src/structures/for_logic/Implication.py
@@ -1,6 +1,21 @@
 from .Sentence import Sentence
 
 class Implication(Sentence):
-    def __init__(self):
-        pass
+    def __init__(self, antecedent, consequent):
+        Sentence.validate(antecedent)
+        self.antecedent = antecedent
+        
+        Sentence.validate(consequent)
+        self.consequent = consequent
+        
+        return
+    
+    def __eq__(self, other):
+        return (isinstance(other, Implication)) and ((self.antecedent == other.antecedent) and (self.consequent == other.consequent))
+    
+    def __hash__(self):
+        return hash(("implies", hash(self.antecedent), hash(self.consequent)))
+    
+    def __repr__(self):
+        return f"Implication({self.antecedent}, {self.consequent})"
     
