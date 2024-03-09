fd96d0d784a3432a5bf6835933e254d6b3f732ce
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-03-09 17:53:27 +0300

    restructure the structure folder for reducing complexity

--- Artificial Intelligence Applications Project Collection/src/structures/__init__.py
+++ Artificial Intelligence Applications Project Collection/src/structures/__init__.py
@@ -1,6 +1,6 @@
-from .Node import Node
-from .Maze import Maze
-from .Queue import Queue
-from .Stack import Stack
+from .for_search.Node import Node
+from .for_search.Maze import Maze
+from .for_search.Queue import Queue
+from .for_search.Stack import Stack
 
-from .TicTacToe import TicTacToe
+from .for_minimax.TicTacToe import TicTacToe
