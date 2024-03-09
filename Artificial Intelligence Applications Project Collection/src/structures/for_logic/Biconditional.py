272d560be5b8d3efaae0d26c79e52c61aabb775b
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-03-09 19:58:08 +0300

    add Biconditional class helper function implementations

--- Artificial Intelligence Applications Project Collection/src/structures/for_logic/Biconditional.py
+++ Artificial Intelligence Applications Project Collection/src/structures/for_logic/Biconditional.py
@@ -1,6 +1,21 @@
 from .Sentence import Sentence
 
 class Biconditional(Sentence):
-    def __init__(self):
-        pass
+    def __init__(self, left, right):
+        Sentence.validate(left)
+        self.left = left
+        
+        Sentence.validate(right)
+        self.right = right
+        
+        return
+    
+    def __eq__(self, other):
+        return (isinstance(other, Biconditional) and ((self.left == other.left) and (self.right == other.right)))
+    
+    def __hash__(self):
+        return (("iff", hash(self.left), hash(self.right)))
+    
+    def __repr__(self):
+        return f"Biconditional({self.left}, {self.right})"
     
