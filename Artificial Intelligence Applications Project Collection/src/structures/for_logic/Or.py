8cf02ca25b9acc81bd099141b4d63577a081da82
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-03-09 18:38:46 +0300

    add Or class __init__ function implementation

--- Artificial Intelligence Applications Project Collection/src/structures/for_logic/Or.py
+++ Artificial Intelligence Applications Project Collection/src/structures/for_logic/Or.py
@@ -1,6 +1,10 @@
 from .Sentence import Sentence
 
 class Or(Sentence):
-    def __init__(self):
-        pass
+    def __init__(self, *disjuncts):
+        for disjunct in disjuncts:
+            Sentence.validate(disjunct)
+            
+        self.disjuncts = list(disjuncts)
+        return
     
