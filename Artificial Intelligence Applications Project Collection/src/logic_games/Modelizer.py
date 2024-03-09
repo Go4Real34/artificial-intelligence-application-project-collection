01cd8999e7a13816172ee40580d6d180e10abc01
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-03-09 20:21:28 +0300

    add Modelizer class logic statement evaluating function implementations

--- Artificial Intelligence Applications Project Collection/src/logic_games/Modelizer.py
+++ Artificial Intelligence Applications Project Collection/src/logic_games/Modelizer.py
@@ -4,3 +4,29 @@ class Modelizer:
         self.query = query
         return
     
+    def check_all(self, symbols, model):
+        if not symbols:
+            if self.knowledge.evaluate(model):
+                return self.query.evaluate(model)
+            
+            return True
+        
+        else:
+            remaining = symbols.copy()
+            popped = remaining.pop()
+            
+            model_true = model.copy()
+            model_true[popped] = True
+            
+            model_false = model.copy()
+            model_false[popped] = False
+            
+            return ((self.check_all(remaining, model_true)) and (self.check_all(remaining, model_false)))
+        
+    def get_all_symbols_combined(self):
+        return set.union(self.knowledge.symbols(), self.query.symbols())
+    
+    def check(self):
+        symbols = self.get_all_symbols_combined()
+        return self.check_all(symbols, dict())
+    
