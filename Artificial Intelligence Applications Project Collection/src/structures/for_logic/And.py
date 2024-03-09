95689ffa602018a82e41fd3f581721fcba82ac71
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-03-09 19:31:54 +0300

    add And class helper function implementations

--- Artificial Intelligence Applications Project Collection/src/structures/for_logic/And.py
+++ Artificial Intelligence Applications Project Collection/src/structures/for_logic/And.py
@@ -1,6 +1,19 @@
 from .Sentence import Sentence
 
 class And(Sentence):
-    def __init__(self):
-        pass
+    def __init__(self, *conjuncts):
+        for conjunct in conjuncts:
+            Sentence.validate(conjunct)
+        self.conjuncts = list(conjuncts)
+        return
+    
+    def __eq__(self, other):
+        return (isinstance(other, And)) and self.conjunct == other.conjuncts
+    
+    def __hash__(self):
+        return hash(("and", tuple(hash(conjunct) for conjunct in self.conjuncts)))
+    
+    def __repr__(self):
+        conjuctions = ", ".join([str(conjunct) for conjunct in self.conjuncts])
+        return f"And({conjuctions})"
     
