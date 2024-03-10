8273daf90d852ec863ba0ee2f4f9ea3c047930e0
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-03-10 23:07:11 +0300

    initialize MASTERMIND class which derives from Problemizer

--- Artificial Intelligence Applications Project Collection/src/projects/__init__.py
+++ Artificial Intelligence Applications Project Collection/src/projects/__init__.py
@@ -7,3 +7,4 @@ from .minimax_agent import MINIMAX
 from .logic_games import RAINY_DAY
 from .logic_games import HOGWARTS_HOUSES
 from .logic_games import WHO_IS_THE_MURDERER
+from .logic_games import MASTERMIND
---
+++ Artificial Intelligence Applications Project Collection/src/projects/logic_games/MASTERMIND.py
@@ -0,0 +1,7 @@
+from ...structures import Problemizer
+
+class MASTERMIND(Problemizer):
+    def __init__(self):
+        super().__init__()
+        pass
+    
