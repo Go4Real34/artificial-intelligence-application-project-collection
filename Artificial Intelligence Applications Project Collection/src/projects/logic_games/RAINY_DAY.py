0a6c6866e1f41da861edd610af8310c0068b96c4
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-03-10 03:28:18 +0300

    add RAINY_DAY class introduce_problem function implementation

--- Artificial Intelligence Applications Project Collection/src/projects/logic_games/RAINY_DAY.py
+++ Artificial Intelligence Applications Project Collection/src/projects/logic_games/RAINY_DAY.py
@@ -8,6 +8,15 @@ class RAINY_DAY:
         self.knowledge = None
         return
     
+    def introduce_problem(self):
+        print("Welcome to the rainy day problem.")
+        print("The problem is as follows:")
+        print("If it is not raining, Hagrid will be happy.")
+        print("Either Hagrid or Dumbledore is happy.")
+        print("But Hagrid and Dumbledore cannot be sad at the same time.")
+        print("Create the correct knowledge base to solve the problem.")
+        return
+    
     def check_result(self):
         print(f"Current Knowledge Base: {self.knowledge.formula()}")
         model = Modelizer(self.knowledge, self.rain)
