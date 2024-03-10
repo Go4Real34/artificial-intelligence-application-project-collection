17c2ed6ed1a1e0cfc4225b97e06a7fa0d7cf3a2b
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-03-10 22:48:28 +0300

    add WHO_IS_THE_MURDERER add information function implementation

--- Artificial Intelligence Applications Project Collection/src/projects/logic_games/WHO_IS_THE_MURDERER.py
+++ Artificial Intelligence Applications Project Collection/src/projects/logic_games/WHO_IS_THE_MURDERER.py
@@ -52,3 +52,8 @@ class WHO_IS_THE_MURDERER(Problemizer):
         print("The problem is as follows:")
         return
     
+    def add_information(self):
+        operation_index, operands_index, should_save_to_knowledge_base = self.get_information()
+        self.add_logical_expression_to_knowledge_base(operation_index, operands_index, should_save_to_knowledge_base)
+        return
+    
