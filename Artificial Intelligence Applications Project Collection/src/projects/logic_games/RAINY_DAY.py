90e9a83ce684407b70bb22fea45d0c6084a4f2a9
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-03-10 03:24:18 +0300

    add RAINY_DAY class __init__ function implementation

--- Artificial Intelligence Applications Project Collection/src/projects/logic_games/RAINY_DAY.py
+++ Artificial Intelligence Applications Project Collection/src/projects/logic_games/RAINY_DAY.py
@@ -1,4 +1,10 @@
+from ...structures import Symbol, Modelizer
+
 class RAINY_DAY:
     def __init__(self):
-        pass
+        self.rain = Symbol("Rain")
+        self.hagrid = Symbol("Hagrid")
+        self.dumbledore = Symbol("Dumbledore")
+        self.knowledge = None
+        return
     
