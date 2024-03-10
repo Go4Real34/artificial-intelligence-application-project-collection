ea5d349b7f2d9e697e02a37d300f6e27c534b39c
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-03-10 23:23:26 +0300

    add MASTERMIND class __init__ function implementation

--- Artificial Intelligence Applications Project Collection/src/projects/logic_games/MASTERMIND.py
+++ Artificial Intelligence Applications Project Collection/src/projects/logic_games/MASTERMIND.py
@@ -1,7 +1,33 @@
-from ...structures import Problemizer
+from ...structures import Problemizer, Symbol
 
 class MASTERMIND(Problemizer):
     def __init__(self):
         super().__init__()
-        pass
+        
+        self.colors = ["Red", "Blue", "Green", "Yellow"]
+        self.positions = [1, 2, 3, 4]
+        self.symbols = []
+        for position in range(4):
+            for color in self.colors:
+                for position in self.positions:
+                    self.symbols.append(Symbol(f"{color} {position}"))
+                
+        self.operations = {
+            1: "And",
+            2: "Or",
+            3: "Not",
+            4: "Implication",
+            5: "Biconditional",
+            6: "Add Knowledge to Knowledge Base from Extra Added Operands",
+            7: "Clear Knowledge Base",
+            8: "Clear Extra Added Operands"
+        }
+        
+        self.operands = {}
+        for index, symbol in enumerate(self.symbols):
+            self.operands.update({
+                index + 1: [str(symbol), symbol]
+            })
+
+        return
     
