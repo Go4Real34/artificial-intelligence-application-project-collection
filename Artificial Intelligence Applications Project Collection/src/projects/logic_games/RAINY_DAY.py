7884bd0f4370ef61e836e00de30165e1ebdf511a
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-03-10 20:06:09 +0300

    move necessary functions from Problemizer to RAINY_DAY

--- Artificial Intelligence Applications Project Collection/src/projects/logic_games/RAINY_DAY.py
+++ Artificial Intelligence Applications Project Collection/src/projects/logic_games/RAINY_DAY.py
@@ -1,4 +1,4 @@
-from ...structures import Problemizer, Symbol
+from ...structures import Problemizer, Symbol, Modelizer, And, Or, Not, Implication, Biconditional
 
 class RAINY_DAY(Problemizer):
     def __init__(self):
@@ -30,7 +30,7 @@ class RAINY_DAY(Problemizer):
         while True:
             self.print_problem_information()
             self.add_information()
-            if self.check_result(self.knowledge, self.rain):
+            if self.check_result():
                 break
             
         return
@@ -49,6 +49,52 @@ class RAINY_DAY(Problemizer):
         print("The problem is as follows:")
         return
     
+    def add_information(self):
+        operation_index, operands_index, should_save_to_knowledge_base = self.get_information()
+        if operation_index == len(self.operations) - 2 and operands_index != [0] and should_save_to_knowledge_base:
+            self.knowledge.add(self.operands[operands_index[0]][1])
+            self.knowledge_length += 1
+            return
+
+        elif operation_index == len(self.operations) - 1 and operands_index == [0] and should_save_to_knowledge_base:
+            self.clear_knowledge_base()
+            print("\nKnowledge base is cleared.")
+            return
+        
+        elif operation_index == len(self.operations) and operands_index == [0] and not should_save_to_knowledge_base:
+            self.clear_extra_added_operands()
+            print("\nExtra Added Operands are cleared.")
+            return
+        
+        thing_to_add = None
+        if self.operations[operation_index] == "Not":
+            thing_to_add = Not(self.operands[operands_index[0]][1])
+            
+        elif self.operations[operation_index] == "Implication":
+            thing_to_add = Implication(self.operands[operands_index[0]][1], self.operands[operands_index[1]][1])
+            
+        elif self.operations[operation_index] == "Biconditional":
+            thing_to_add = Biconditional(self.operands[operands_index[0]][1], self.operands[operands_index[1]][1])
+            
+        elif self.operations[operation_index] == "Or":
+            or_statement = Or()
+            for operand in operands_index:
+                or_statement.add(self.operands[operand][1])  
+            thing_to_add = or_statement
+            
+        elif self.operations[operation_index] == "And":
+            and_statement = And()
+            for operand in operands_index:
+                and_statement.add(self.operands[operand][1])  
+            thing_to_add = and_statement
+            
+        self.operands.update({len(self.operands) + 1: [thing_to_add.formula(), thing_to_add]})
+        if should_save_to_knowledge_base:
+            self.knowledge.add(thing_to_add)
+            self.knowledge_length += 1
+        
+        return
+    
     def clear_extra_added_operands(self):
         self.operands = {
             1: ["Rain", self.rain],
@@ -58,3 +104,18 @@ class RAINY_DAY(Problemizer):
         
         return
     
+    def check_result(self):
+        print(f"\nCurrent Knowledge Base: " + (f"{self.knowledge.formula()}" if self.knowledge_length != 0 else "None"), end='\n' * 2)
+        
+        if self.knowledge_length != 0:
+            model = Modelizer(self.knowledge, self.rain)
+            is_knowledge_base_correct = model.check()
+            if is_knowledge_base_correct:
+                print("Correct knowledge base acquired. Well done!")
+            
+            else:
+                print("Knowledge base is still missing some information. Keep trying!")
+                
+            return is_knowledge_base_correct
+            
+        return False
--- Artificial Intelligence Applications Project Collection/src/structures/for_logic/Problemizer.py
+++ Artificial Intelligence Applications Project Collection/src/structures/for_logic/Problemizer.py
@@ -1,10 +1,4 @@
-from .Symbol import Symbol
-from .Modelizer import Modelizer
 from .And import And
-from .Or import Or
-from .Not import Not
-from .Implication import Implication
-from .Biconditional import Biconditional
 
 class Problemizer:
     def __init__(self):
@@ -14,10 +8,6 @@ class Problemizer:
         self.knowledge = And()
         self.knowledge_length = 0
         
-        return
-    
-    def introduce_problem(self):
-        raise Exception("No problem exists.")
         return
     
     def get_information(self):
@@ -82,7 +72,7 @@ class Problemizer:
             print("Invalid input type on operand selection.")
             exit(2)
             
-        if selected_operand_indexes == [len(self.operands)]:
+        if selected_operation_index == len(self.operations) - 2:
             return selected_operation_index, selected_operand_indexes, True
         
         print("\nSelect the place to save this statement.")
@@ -106,74 +96,9 @@ class Problemizer:
             exit(2)
             
         return selected_operation_index, selected_operand_indexes, (selected_place_index == 1)
-
-    def add_information(self):
-        operation_index, operands_index, should_save_to_knowledge_base = self.get_information()
-        if operation_index == len(self.operations) - 2 and operands_index != [0] and should_save_to_knowledge_base:
-            self.knowledge.add(self.operands[operands_index[0]][1])
-            self.knowledge_length += 1
-            return
-
-        elif operation_index == len(self.operations) - 1 and operands_index == [0] and should_save_to_knowledge_base:
-            self.clear_knowledge_base()
-            print("\nKnowledge base is cleared.")
-            return
-        
-        elif operation_index == len(self.operations) and operands_index == [0] and not should_save_to_knowledge_base:
-            self.clear_extra_added_operands()
-            print("\nExtra Added Operands are cleared.")
-            return
-        
-        thing_to_add = None
-        if self.operations[operation_index] == "Not":
-            thing_to_add = Not(self.operands[operands_index[0]][1])
-            
-        elif self.operations[operation_index] == "Implication":
-            thing_to_add = Implication(self.operands[operands_index[0]][1], self.operands[operands_index[1]][1])
-            
-        elif self.operations[operation_index] == "Biconditional":
-            thing_to_add = Biconditional(self.operands[operands_index[0]][1], self.operands[operands_index[1]][1])
-            
-        elif self.operations[operation_index] == "Or":
-            or_statement = Or()
-            for operand in operands_index:
-                or_statement.add(self.operands[operand][1])  
-            thing_to_add = or_statement
-            
-        elif self.operations[operation_index] == "And":
-            and_statement = And()
-            for operand in operands_index:
-                and_statement.add(self.operands[operand][1])  
-            thing_to_add = and_statement
-            
-        self.operands.update({len(self.operands) + 1: [thing_to_add.formula(), thing_to_add]})
-        if should_save_to_knowledge_base:
-            self.knowledge.add(thing_to_add)
-            self.knowledge_length += 1
-        
-        return
     
     def clear_knowledge_base(self):
         self.knowledge = And()
         self.knowledge_length = 0
         return
-    
-    def clear_extra_added_operands(self):
-        raise Exception("No extra added knowledge exists.")
-    
-    def check_result(self, knowledge, model):
-        print(f"\nCurrent Knowledge Base: " + (f"{self.knowledge.formula()}" if self.knowledge_length != 0 else "None"), end='\n' * 2)
-        
-        if self.knowledge_length != 0:
-            model = Modelizer(knowledge, model)
-            is_knowledge_base_correct = model.check()
-            if is_knowledge_base_correct:
-                print("Correct knowledge base acquired. Well done!")
-            
-            else:
-                print("Knowledge base is still missing some information. Keep trying!")
-                
-            return is_knowledge_base_correct
-            
-        return False
     
